b743289bb229d47d03392873160e6ab398b2ad56
玉川隆二 <homes31285@gmail.com>
2023-09-14 11:30:37 +0900

    テンプレートエンジンの活用

--- src/main/java/com/example/practice/web/indexController.java
+++ src/main/java/com/example/practice/web/indexController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class indexController {
     @GetMapping
-    @ResponseBody
     public String index() {
-        return "<h1>Hello World!<h1>";
+        return "index";
     }
 }
